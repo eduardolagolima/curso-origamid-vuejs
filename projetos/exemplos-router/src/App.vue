ac8150e9774a40c17b98fc229d28125351066875
eduardolagolima <eduardolago95@hotmail.com>
2019-12-02 23:58:40 -0300

    Iniciando módulo de router

---
+++ projetos/exemplos-router/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>App</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/cursos">Cursos</router-link>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+a {
+  padding: 10px;
+}
+
+.router-link-exact-active {
+  color: #84e;
+  font-weight: bold;
+}
+</style>
---
+++ projetos/exemplos-router/src/views/Cursos.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Cursos</h1>
+</template>
+
+<script>
+export default {
+  name: 'Cursos'
+}
+</script>
+
+<style>
+
+</style>
