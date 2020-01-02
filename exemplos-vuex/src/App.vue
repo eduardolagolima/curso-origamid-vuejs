442524e6e2bc208cc63a6abe2f1b5d27c03d0210
eduardolagolima <eduardolago95@hotmail.com>
2020-01-02 09:48:04 -0300

    Actions

--- exemplos-vuex/src/App.vue
+++ exemplos-vuex/src/App.vue
@@ -3,18 +3,25 @@
     <User/>
     <hr>
     <Curso/>
+    <hr>
+    <pre>{{$store.state.cep}}</pre>
   </div>
 </template>
 
 <script>
 import User from "@/views/User.vue";
 import Curso from "@/views/Curso.vue";
+import { mapActions } from "vuex";
 
 export default {
   name: "app",
   components: {
     User,
     Curso
+  },
+  methods: mapActions(["fetchCep"]),
+  created() {
+    this.fetchCep()
   }
 };
 </script>
--- exemplos-vuex/src/store.js
+++ exemplos-vuex/src/store.js
@@ -6,7 +6,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     usuario: "Eduardo",
-    quantidadeAulasCompletas: 15
+    quantidadeAulasCompletas: 15,
+    cep: null
   },
   mutations: {
     MUDAR_USUARIO(state, payload) {
@@ -14,6 +15,27 @@ export default new Vuex.Store({
     },
     COMPLETAR_AULA(state) {
       state.quantidadeAulasCompletas++;
+    },
+    SETAR_CEP(state, payload) {
+      state.cep = payload
+    }
+  },
+  actions: {
+    completarAula(context) {
+      context.commit("COMPLETAR_AULA")
+    },
+    fetchCep(context) {
+      fetch(`https://viacep.com.br/ws/99150000/json`)
+        .then(r => r.json())
+        .then(r => {
+          setTimeout(() => {
+            context.commit("SETAR_CEP", r);
+            context.commit("MUDAR_USUARIO", {
+              novoUsuario: "Ciclano"
+            });
+            context.dispatch("completarAula");
+          }, 1000)
+        });
     }
   }
 });
