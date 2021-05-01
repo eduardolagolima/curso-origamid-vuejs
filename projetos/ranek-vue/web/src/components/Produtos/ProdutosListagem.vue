814b10539897bc7f49da069f4092f3f48868db3e
eduardolagolima <eduardolagolima@gmail.com>
2021-05-01 15:25:21 -0300

    adiciona estilo na listagem de produtos

--- projetos/ranek-vue/web/src/components/Produtos/ProdutosListagem.vue
+++ projetos/ranek-vue/web/src/components/Produtos/ProdutosListagem.vue
@@ -1,21 +1,34 @@
 <template>
   <section class="produtos-container">
     <div
-      v-for="({fotos, preco, nome, descricao}, index) in produtos"
-      :key="index"
+      v-if="produtos && produtos.length > 0"
+      class="produtos"
     >
-      <img
-        v-if="fotos.length"
-        :src="fotos[0].src"
-        :alt="fotos[0].titulo"
+      <div
+        v-for="({fotos, preco, nome, descricao}, index) in produtos"
+        :key="index"
+        class="produto"
       >
-      <p class="preco">
-        {{ preco }}
+        <router-link to="/">
+          <img
+            v-if="fotos.length"
+            :src="fotos[0].src"
+            :alt="fotos[0].titulo"
+          >
+          <p class="preco">
+            {{ preco }}
+          </p>
+          <h2 class="titulo">
+            {{ nome }}
+          </h2>
+          <p>{{ descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">
+        Nenhum resultado encontrado para "{{ this.$route.query.q }}"
       </p>
-      <h2 class="titulo">
-        {{ nome }}
-      </h2>
-      <p>{{ descricao }}</p>
     </div>
   </section>
 </template>
@@ -58,5 +71,49 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>
