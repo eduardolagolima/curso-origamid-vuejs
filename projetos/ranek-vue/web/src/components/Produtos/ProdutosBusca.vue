2db56baea007519abab5b30d057df841b1c58cd6
eduardolagolima <eduardolagolima@gmail.com>
2021-05-01 15:36:59 -0300

    adiciona estilo no campo de busca

--- projetos/ranek-vue/web/src/components/Produtos/ProdutosBusca.vue
+++ projetos/ranek-vue/web/src/components/Produtos/ProdutosBusca.vue
@@ -5,11 +5,12 @@
       v-model="busca"
       name="busca"
       type="text"
+      placeholder="Buscar..."
     >
     <input
       id="lupa"
       type="submit"
-      value="Buscar"
+      value=""
       @click.prevent="buscarProdutos"
     >
   </form>
@@ -32,3 +33,34 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../../assets/search.svg") no-repeat center center;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+  box-shadow: none;
+}
+</style>
