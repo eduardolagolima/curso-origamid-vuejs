53c9ee6c3880ad9fae13036241049a4081d4d9ea
eduardolagolima <eduardolagolima@gmail.com>
2021-05-01 21:23:56 -0300

    corrige bug na paginação

--- projetos/ranek-vue/web/src/helpers/index.js
+++ projetos/ranek-vue/web/src/helpers/index.js
@@ -2,7 +2,8 @@ const serializeObject = (object) => Object.keys(object).reduce((acc, cur) => (
   [...acc, `${cur}=${object[cur]}`]
 ), []).join('&');
 
-const createArrayRangeByNumber = (length, referenceNumber, minValue, maxValue) => {
+const createArrayRangeByNumber = (number, referenceNumber, minValue, maxValue) => {
+  const length = Math.min(number, maxValue);
   let quantityPerSide = referenceNumber - Math.trunc(length / 2);
   quantityPerSide = Math.max(quantityPerSide, minValue);
   quantityPerSide = Math.min(quantityPerSide, maxValue - length);
