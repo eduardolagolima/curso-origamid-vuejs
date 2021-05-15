fcb319b8eb6f28f0a97dc75f03dc34c6583f6e84
eduardolagolima <eduardolagolima@gmail.com>
2021-05-15 20:32:51 -0300

    adiciona edição de perfil do usuário

--- projetos/ranek-vue/web/src/views/User/UserEditProfile.vue
+++ projetos/ranek-vue/web/src/views/User/UserEditProfile.vue
@@ -1,10 +1,40 @@
 <template>
-  <p>UserEditProfile</p>
+  <section>
+    <UserForm>
+      <button
+        class="btn"
+        @click.prevent="editProfile"
+      >
+        Editar
+      </button>
+    </UserForm>
+  </section>
 </template>
 
 <script>
+import UserForm from '../../components/User/UserForm.vue';
+
+import api from '../../services/api';
+
 export default {
   name: 'UserEditProfile',
+  components: {
+    UserForm,
+  },
+  methods: {
+    async editProfile() {
+      try {
+        await api.put(
+          `/users/${this.$store.state.user.id}`,
+          this.$store.state.user,
+        );
+        this.$store.dispatch('getUser');
+        this.$router.push({ name: 'user-products' });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 
